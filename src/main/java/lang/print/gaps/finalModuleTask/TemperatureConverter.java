e171402ef5edf9c8d380ca2a7877eeb69b984986
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-14 23:07:59 +0400

    Made a final task for module 3

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        int fahrenheit = (int) ((temperatureCelsius * 1.8) + 32);
+        System.out.println(fahrenheit);
     }
 }
